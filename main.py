053b30f8d412a052180754881c67c7b5d2ce8d77
SoledadRozzatti <soledadrozzatti@gmail.com>
2023-05-16 06:33:30 -0300

    cambio variables

--- main.py
+++ main.py
@@ -110,7 +110,7 @@ def retorno(pelicula: str):
 
 df_m = pd.read_csv('data_modelo.csv', sep=',')
 df_m = df_m.sample(n=20000, random_state=42)
-punt_matrix = df_m.pivot_table(index='title', values='puntaje_promedio')
+punt_matrix = df_m.pivot_table(index='title', values='vote_average')
 simil_matrix = cosine_similarity(punt_matrix.fillna(0))
 
 @app.get('/recomendacion/{titulo}')
